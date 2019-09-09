d17d79dee6fdb79e2026fdc167417bc0b32308f1
Oscar <oscar.e.chavarria.campos@intel.com>
2019-09-08 21:04:06 -0600

    TEC-Intel

---
+++ Scripting_Essentials_WorkShop.ps1
@@ -0,0 +1,100 @@
+﻿############################################
+# Workshop PowerShell Examples 
+# Intel 
+###########################################
+
+# Windows Examples: 
+#Ipconfig/Ping/netstat
+#Runner: notepad/ taskmgr / services.msc
+#cd, del, xcopy
+#Run .exe
+
+# Alias Examples: 
+
+#get-childitem C:\ | foreach-object -process { $_.length / 1024 }
+#get-childitem C:\ | % -process { $_.length / 1024 }
+
+#Set-Location C:\
+# cd C:\ 
+
+# md workshop
+
+# cat  / Get-Content
+# clear / cls  / Clear-Host
+# cd / chdir / 	Set-Location
+# ft / format table 
+# erase	/ Remove-Item
+# ?	/ Where-Object
+# % / Foreach-Object
+# md / mkdir
+
+
+
+
+#stucture examples. 
+# Operators
+# conditionals: 
+# -eq	equals
+# -ne	not equals
+# -gt	greater than
+# -ge	greater than or equal
+# -lt	less than
+# -le	less than or equal
+
+#Matching	
+# -like	/ "PowerShell" -like "*shell" || "PowerShell", "Server" -like "*shell"
+
+#Containment	
+# -contains	/ @(1,2,3) -contains 1
+
+#Replacement 	
+# -replace / 'UCR' -replace 'UCR','TEC'
+
+#Type	
+# -is / 1 -is [int] || 1 -is $var.getType()
+
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-6
+
+# Statements: 
+# if: 
+#if(5 -lt 4 ){ echo 5}
+#else{echo 4} 
+
+
+# Defining Objects: 
+# Variable: $var_Name
+# Array: @(1,2,3)
+
+# Class
+<# 
+class Pokemon {
+    [string]$Name
+    [string]$Type
+    [string]$Type2 
+}
+#>
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_classes?view=powershell-6
+
+
+#cmdlet Examples ( verb-noun ) : 
+
+# get-alias
+# get-help command / get-help command % || get-help get-alias
+# Get-PSSnapin 
+
+
+# get-childitem C:\ 
+
+# $events = get-eventlog -logname system -newest 1000
+# $events | foreach-object -process { Write-Host $_.Source} 
+
+# Pipeline
+# Get-Service -Name *pool* | stop-service
+# dir | out-file -filepath c:\workshop\folderinfo.txt
+
+# Practice:  
+# 1 - create a folder name workshop 
+# 2 - search for the alias to create a file. 
+# 3 - create the file. 
+# 4 - search for the alias to delete a file. 
+# 5 - Delete the created file.  
---
+++ Scripting_Essentials_WorkShop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+# ls  -l / display the content of the folder with details. 
+# man / help ( not working for mingw )
+# touch / Update the access and modification times / Creates an empty file
+# cp targetfile newfile / copies a file
+# rm / removes a file. 
+# cat / Check the content of a file. 
+# more ( not working for mingw )
+# mkdir 
+# rmdir 
+# grep Search for PATTERN in each FILE or standard input.
+# Command --help / for see the full output on how to use the command. 
+
+
+# Interpolation 
+#In computer programming, string interpolation (or variable interpolation, variable substitution, 
+#or variable expansion) is the process of evaluating a string literal containing one or more placeholders. 
+
+# touch hello{1..10}.txt
+# echo  {A..z}
+# echo  {A..z..2}
+# Check for the sort error. 
+
+#Redirecting with > . 
+# echo {A..z} > abc.txt
+# echo {A..z} 1> good.txt 2> failed.txt / 1 means success and 2 means failures. & means both. 
+
+# Pipeline 
+# touch hello.txt | cp hello.txt copyhello.txt | rm hello.txt
+
+
+
+
